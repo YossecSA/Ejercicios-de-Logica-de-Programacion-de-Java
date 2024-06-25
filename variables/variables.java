e767ae806b59e7e042b074edff6d5a15241257dd
Yossec <yosec.arratea22@gmail.com>
2024-06-25 11:41:03 -0500

    variables basicos para usar en java

---
+++ variables/variables.java
@@ -0,0 +1,40 @@
+package variables;
+
+public class variables {
+    public static void main(String[] args) {
+
+        /*
+         * Tipo de Variables
+         * Variables Primitivas: Almacena valores basicos
+         * variables de referencia: Almacena direcciones de memoria que apunta a objetos
+         */
+
+        // Primitivos
+
+        // Enteros
+        int numero = 129;
+
+        // Flotantes
+        float flotantes = 1.25f;
+        double decimales = 5.01234567899;
+
+        // Booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        // Caracteres
+        char letra = 'A';
+
+        // Referencia
+        String cadena = "Hola mundo";
+
+        // Llamada de las variables
+        System.out.println(numero);
+        System.out.println(flotantes);
+        System.out.println(decimales);
+        System.out.println(verdadero);
+        System.out.println(falso);
+        System.out.println(letra);
+        System.out.println(cadena);
+    }
+}
